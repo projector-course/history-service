9213746b0f9eaa913e3079f2bb2c0350ab8d0a49
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2021-11-08 18:29:20 +0200

    feat: create service structure + rote health

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const HOST = '127.0.0.1';
+const PORT = 3003;
+const BASE_URL = `http://${HOST}:${PORT}`;
+const SERVICE_NAME = 'HISTORY SERVICE';
+
+module.exports = {
+  PORT,
+  BASE_URL,
+  SERVICE_NAME,
+};
