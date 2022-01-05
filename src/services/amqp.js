841537ecbff878dfd89c45dbfacefa15d5f41bc9
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-01-05 20:58:31 +0200

    fix: responsibility

--- src/services/amqp.js
+++ src/services/amqp.js
@@ -1,6 +1,8 @@
 const rabbit = require('amqplib');
 const { getModuleLogger } = require('./logService');
-const { AMQP_HOST, AMQP_QUEUE_NAME } = require('./configService');
+const {
+  AMQP_HOST, AMQP_QUEUE_NAME, AMQP_EXCHANGE_NAME, AMQP_EVENT,
+} = require('./configService');
 
 const logger = getModuleLogger(module);
 logger.debug('SERVICE CREATED');
@@ -10,8 +12,11 @@ async function connectAmqp(callback) {
   const channel = await connection.createChannel();
   logger.debug('AMQP channel created');
 
-  channel.consume(AMQP_QUEUE_NAME, (m) => {
-    callback(m.content).then(() => channel.ack(m));
+  const queue = await channel.assertQueue(AMQP_QUEUE_NAME);
+  await channel.bindQueue(queue.queue, AMQP_EXCHANGE_NAME, AMQP_EVENT);
+
+  channel.consume(AMQP_QUEUE_NAME, async (m) => {
+    await callback(m.content).then(() => channel.ack(m));
   });
 }
 
